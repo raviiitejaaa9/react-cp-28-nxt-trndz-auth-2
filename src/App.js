327149f6be0b77f182ae11779ed5f3756c5ab1b0
raviiitejaaa9 <raviiitejaaa9@gmail.com>
2023-06-28 17:24:02 +0000

    fullCode

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import {Route, Switch} from 'react-router-dom'
+
+// import Header from './components/Header'
+import LoginForm from './components/LoginForm'
+import Home from './components/Home'
+import Products from './components/Products'
+import Cart from './components/Cart'
+import NotFound from './components/NotFound'
+
+import './App.css'
+
+const App = () => (
+  <>
+    <Switch>
+      <Route exact path="/login" component={LoginForm} />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/products" component={Products} />
+      <Route exact path="/cart" component={Cart} />
+      <Route component={NotFound} />
+    </Switch>
+  </>
+)
+
+export default App
